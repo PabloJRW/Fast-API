c4f7849e49cd03a0d47f58c676c2305eeeb58f46
Pablo <ramoswilkinspj28@gmail.com>
2023-06-29 00:10:29 -0500

    adding password erfication

--- routers/users.py
+++ routers/users.py
@@ -6,7 +6,7 @@ from database import SessionLocal
 from typing import Annotated
 from sqlalchemy.orm import Session
 from starlette import status
-from .auth import get_current_user
+from routers.auth import get_current_user
 from passlib.context import CryptContext
 
 
@@ -29,6 +29,7 @@ bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 class UserVerification(BaseModel):
     password: str
     new_password: str = Field(min_lenght=6)
+    verify_new_password: str = Field()
 
 
 @router.get("/", status_code=status.HTTP_200_OK)
@@ -47,6 +48,9 @@ async def change_password(user: user_dependency, db: db_dependency,
     user_model = db.query(Users).filter(Users.id == user.get('id')).first()
     if not bcrypt_context.verify(user_verification.password, user_model.hashed_password):
         raise HTTPException(status_code=401)
+
+    if not user_verification.password == user_verification.verify_new_password:
+        raise HTTPException(status_code=400, detail="Passwords do not march!.")
 
     user_model.hashed_password = bcrypt_context.hash(user_verification.new_password)
     db.add(user_model)
